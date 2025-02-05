5f67f9e60d3088d829b7b92d7a7ce346d1ac0e42
oseiasorbilem <50297616+oseiasbenedysorbilem@users.noreply.github.com>
2025-02-04 19:12:42 -0500

    Add files via upload

---
+++ aulas/script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector('.wrapper ');
+const loginlink = document.querySelector('.loginlink ');
+const registerlink = document.querySelector('.register-link ');
+const btnpopup = document.querySelector(',btnloginpopup');
+const iconclose = document.querySelector(',iconclose');
+
+registerlink.addEventListener( cancelIdleCallback('click ',)()=>{
+
+    wrapper.classList.add(' active');
+
+
+});
+
+loginlink.addEventListener( cancelIdleCallback('click ',()=>{
+    
+    wrapper.classList.add(' active');
+
+
+});
+
+btnLoginpopup.addEventListener( cancelIdleCallback('click ',()=>{
+    
+    wrapper.classList.remove(' active-popup');
+
+iconclose.addEventListener( cancelIdleCallback('click ',()=>{
+    
+        wrapper.classList.remove(' active-popup');
